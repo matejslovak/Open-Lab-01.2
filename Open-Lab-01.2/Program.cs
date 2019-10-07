e9da6adf3df0d0c8d5556967a6908f6abf51dbbc
matejslovak <joggurtik1@gmail.com>
2019-10-07 09:52:07 +0200

    iba test zatial

---
+++ Open-Lab-01.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_01._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("iba test ulohy 2");
+        }
+    }
+}
