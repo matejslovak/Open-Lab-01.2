f608fc2b78332d2355e81340539af422a7a918a9
matejslovak <joggurtik1@gmail.com>
2019-10-14 08:47:04 +0200

    Open-Lab-01.2

--- Open-Lab-01.2/Program.cs
+++ Open-Lab-01.2/Program.cs
@@ -4,13 +4,24 @@ namespace Open_Lab_01._2
 {
     class Program
     {
+        public static int Sum(int fnum, int snum)
+        {
+            return (fnum + snum);
+        }
+
         static void Main(string[] args)
         {
-            int a = 5;
-            int b = 20;
-            int c = a + b;
-            Console.WriteLine(c);
+            int fnum;
+            int snum;
+            int sum;
+            Console.WriteLine("Enter the first number");
+            fnum = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("Enter the second number");
+            snum = int.Parse(Console.ReadLine());
+
+            sum = Sum(fnum, snum);
+            Console.WriteLine($"Sum is { sum } ");
 
         }
     }
